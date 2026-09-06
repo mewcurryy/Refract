e71187b2703056e00c4639f37fba318f12853900
Davin P <davinpidada2005@gmail.com>
2026-09-06 22:16:43 +0700

    feat: add HomeViewModel with reactive progress subscription, fully unit tested

--- Refract/Features/Home/HomeViewModel.swift
+++ Refract/Features/Home/HomeViewModel.swift
@@ -5,3 +5,40 @@
 //  Created by Davin P on 03/09/26.
 //
 
+import UIKit
+
+final class HomeViewModel {
+    let userName: String = "Davin P"
+    let greetingSubtitle: String = "Let's coloring your image today!"
+    
+    private let progressStore: ModuleProgressStore
+    private let allModules: [GradingParameter] = GradingParameterCatalog.all
+    
+    var onProgressUpdated: (() -> Void)?
+    
+    init(progressStore: ModuleProgressStore = .shared) {
+        self.progressStore = progressStore
+        self.progressStore.onProgressChanged = { [weak self] in
+            self?.onProgressUpdated?() // UI refresh layar
+        }
+    }
+    
+    var numberOfModules: Int {allModules.count}
+    
+    func module(at index: Int) -> GradingParameter { // untuk ambil data GradingParameter di index ke-i
+        allModules[index]
+    }
+    
+    func isModuleComplete(at index: Int) -> Bool {
+        progressStore.isComplete(allModules[index].id)
+    }
+    
+    var completedModules: [GradingParameter] { // untuk complete modules di Home
+        allModules.filter {progressStore.isComplete($0.id)}
+    }
+    
+    // menampilkan module detail dari module yang dipilih
+    func makeModuleDetailViewModel(at index: Int, sampleImage: UIImage) -> TutorialModuleViewModel {
+        TutorialModuleViewModel(paramsInfo: allModules[index], sampleImage: sampleImage, progressStore: progressStore, allModules: allModules)
+    }
+}
--- RefractTests/ViewModels/HomeViewModelTests.swift
+++ RefractTests/ViewModels/HomeViewModelTests.swift
@@ -5,3 +5,46 @@
 //  Created by Davin P on 03/09/26.
 //
 
+import XCTest
+@testable import Refract
+
+final class HomeViewModelTests: XCTestCase {
+    private var testDefaults: UserDefaults!
+    private var testStore: ModuleProgressStore!
+    private var testViewModel: HomeViewModel!
+    
+    override func setUp() {
+        super.setUp()
+        testDefaults = UserDefaults(suiteName: #file)
+        testDefaults.removePersistentDomain(forName: #file)
+        testStore = ModuleProgressStore(defaults: testDefaults)
+        testViewModel = HomeViewModel(progressStore: testStore)
+    }
+    
+    override func tearDown() {
+        testDefaults.removePersistentDomain(forName: #file)
+        super.tearDown()
+    }
+    
+    func test_numberOfModules_matchesCatalog() {
+        XCTAssertEqual(testViewModel.numberOfModules, GradingParameterCatalog.all.count)
+    }
+    
+    func test_completedModules_isEmpty_whenNothingCompleted() {
+        XCTAssertTrue(testViewModel.completedModules.isEmpty)
+    }
+    
+    func test_completedModules_includeCompletedModule() {
+        testStore.markComplete(.brightness)
+        testStore.markComplete(.vibrance)
+        XCTAssertEqual(testViewModel.completedModules.map(\.id), [.brightness, .vibrance])
+    }
+    
+    func test_onProgressUpdated_calledWhenProgressStoreChanges() {
+        var wasCalled = false
+        testViewModel.onProgressUpdated = { wasCalled = true } // kalau ada progress, ubah called jadi true
+        
+        testStore.markComplete(.contrast)
+        XCTAssertTrue(wasCalled) // wasCalled harus true karena ada markComplete -> harusnya onProgressUpdated ke trigger
+    }
+}
