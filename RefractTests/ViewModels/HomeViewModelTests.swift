6d0116dc562d3a381ba636f8f5c77af0d619d480
Davin P <davinpidada2005@gmail.com>
2026-09-10 11:48:07 +0700

    correct the testcase in HomeViewModelTests

--- RefractTests/ViewModels/HomeViewModelTests.swift
+++ RefractTests/ViewModels/HomeViewModelTests.swift
@@ -31,7 +31,7 @@ final class HomeViewModelTests: XCTestCase {
     }
     
     func test_completedModules_isEmpty_whenNothingCompleted() {
-        XCTAssertFalse(testViewModel.completedModules.isEmpty)
+        XCTAssertTrue(testViewModel.completedModules.isEmpty)
     }
     
     func test_completedModules_includeCompletedModule() {
